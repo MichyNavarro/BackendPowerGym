46ef9a4758ef46a2a5377c5bc7044abe129c0a0d
Pablo Petroni <petronipablo2014@gmail.com>
2024-03-26 18:29:53 -0300

    correccion para deploy

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ const authRoutes = require('./src/routes/auth.routes.js');
 const turnosRoutes = require('./src/routes/turnos.routes.js');
 const usersRoutes = require('./src/routes/users.routes.js');
 const clasesRoutes = require('./src/routes/clases.routes.js');
-<<<<<<< HEAD
-const pagosRoutes = require('./src/routes/pagos.routes.js');
-const adminRoutes = require('./src/routes/adminRouter.js');
-=======
 const comentariosRoutes = require('./src/routes/comentarios.routes.js');
->>>>>>> be259d238148b514b87e0099c21eb8306ed61d6c
 
 require('dotenv').config();
 
@@ -31,12 +26,7 @@ app.use('/api', authRoutes);
 app.use('/api', turnosRoutes);
 app.use('/api', usersRoutes);
 app.use('/api', clasesRoutes);
-<<<<<<< HEAD
-app.use('/api', pagosRoutes);
-app.use('/api', adminRoutes);
-=======
 app.use('/api', comentariosRoutes);
->>>>>>> be259d238148b514b87e0099c21eb8306ed61d6c
 
 async function main() {
     try {
